709ebdc6fc82a2aeca15494d7423dac31814ec11
Brian <brianf.iwnl@gmail.com>
2022-06-26 13:58:38 +1000

    add linkedLists implentation code snippets

---
+++ linkedLists/linkedListPush.js
@@ -0,0 +1,29 @@
+/* Push method linked lists */ 
+
+class Node  {
+    constructor(value) {
+        this.value = value 
+        this.next = null
+    }
+}
+
+class LinkedList { 
+    constructor(value) {
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head 
+        this.length = 1
+    }
+    push(value) {
+        const newNode = new Node(value)
+        if (!this.head) { // empty linkedList edge case 
+            this.head = newNode 
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode 
+            this.tail = newNode
+        }
+        this.length++
+        return this //return whole linkedList
+    }
+}
